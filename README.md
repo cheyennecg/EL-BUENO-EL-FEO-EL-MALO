4194698491d4a60c9bf516984658565dedfc92b7
Cheyenne <cheyennecastrogonzalez@gmail.com>
2026-04-07 12:31:17 +0200

    fix mapa lavanda

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# EL-BUENO-EL-FEO-EL-MALO
-Mini proyecto que hago en mi formación 
+# LA POKEDEX DE LA VISUALIZACIÓN
+
+# 🗺️ Kanto Battle Map:
+
+¡Bienvenido a mi primer proyecto de mapa interactivo de la región de Kanto! Este mapa permite explorar los gimnasios y conocer a sus líderes. A través, de este juego, mi objetivo es aprender a usar filtros, tablas  y crear buenas gráficas. 
+Las pistas de estas últimas las encontrarás en otro la carpeta VISUALIZACION - EL BUENO - EL FEO-EL MALO.
+
+## 🔍 Guía del Entrenador (Pistas)
+* **Ciudad Plateada:** El líder tiene una voluntad de roca. ¿Podrás encontrar su punto débil?
+* **Pueblo Lavanda:** Algo extraño sucede aquí... el mapa se siente diferente en esta zona. ¿Podrás con ello?
+* **Ciudad Celeste:** No te dejes engañar por la belleza del agua, el "Líder" de esta ciudad brilla con luz propia.
+
+## 🛠️ Tecnologías utilizadas
+* **HTML5:** Estructura del mapa y los nodos de los gimnasios.
+* **CSS3:** Diseño retro, rutas (caminos) y posicionamiento absoluto.
+* **JavaScript:** Lógica para actualizar el panel de batalla de forma dinámica.
+* **GoogleSheets:** Para aprender a hacer tablas dinámicas y una buenas gráficas.
+
+## 🚀 Cómo usar el mapa
+1. Abre el enlace del proyecto.
+2. Haz clic en los **puntos rojos** para desplegar la información del líder y sus pistas para saber cuales serán los top 3 pokemos con los que combatir en cada ciudad.
+3. Consulta el panel derecho para ver quién es el Pokémon estrella (**Líder**) de cada gimnasio.
+
+## 👤 Autor
+* **Cheyenne** - *Data Analyst en formación*
--- movimiento.js
+++ movimiento.js
@@ -1,15 +1,15 @@
 
         const data = {
-            pallet: { name: "PUEBLO PALETA", leader: "Cheyenne", type: "INICIO", color: "#ccc", poke: "🐾", as: "Starter", desc: "Aquí comienza tu aventura. ¡Elige bien a tu compañero!" },
+            pallet: { name: "PUEBLO PALETA", leader: "Cheyenne", type: "INICIO", color: "#ccc", poke: "📱", as: "Starter", desc: "Aquí comienza tu aventura. ¡Elige bien a tu compañero!" },
             gym1: { name: "CIUDAD PLATEADA", leader: "Brock", type: "ROCA", color: "var(--rock)", poke: "🐍", as: "Onix", desc: "Ideal para tipos  🌿 o  💧. ¡Tú punto fuerte debe ser el ataque!" },
-            gym2: { name: "CIUDAD CELESTE", leader: "Misty", type: "AGUA", color: "var(--water-type)", poke: "💧", as: "Starmie", desc: "Los tipos ⚡ y  🌿 son tus mejores amigos aquí. Starmie es muy rápido y pega fuerte. Necesitas mucha vida para aguantar el temporal." },
+            gym2: { name: "CIUDAD CELESTE", leader: "Misty", type: "AGUA", color: "var(--water-type)", poke: "🌟", as: "Starmie", desc: "Los tipos ⚡ y  🌿 son tus mejores amigos aquí. Starmie es muy rápido y pega fuerte. Necesitas mucha vida para aguantar el temporal." },
             gym3: { name: "CIUDAD CARMÍN", leader : "Lt.Surge", type: "ELÉCTRICO", color: "var(--electric)", poke: "⚡", as: "Raichu", desc: "Los tipos  🏜️ son inmunes a sus ataques eléctricos. ¡En este caso la defensa  es un buen ataque!" },
             gym4: { name: "CIUDAD AZULONA", leader: "Erika", type: "PLANTA", color: "var(--grass-type)", poke: "🌸", as: "Vileplume", desc: "Líder de planta. Débil contra: Fuego, Volador e Hielo." },
             gym5: { name: "CIUDAD FUCSIA", leader: "Koga", type: "VENENO", color: "var(--poison)", poke: "☁️", as: "Weezing", desc: "Líder de veneno. Débil contra: Psíquico y Tierra. Usa ataques indirectos." },
             gym6: { name: "CIUDAD AZAFRÁN", leader: "Sabrina", type: "PSÍQUICO", color: "var(--psychic)", poke: "🥄", as: "Alakazam", desc: "Líder psíquico. Débil contra: Bicho (técnicamente). ¡Es el gimnasio más difícil!" },
             gym7: { name: "ISLA CANELA", leader: "Blaine", type: "FUEGO", color: "var(--fire)", poke: "🔥", as: "Arcanine", desc: "Líder de fuego. Débil contra: Agua, Tierra y Roca." },
             gym8: { name: "CIUDAD VERDE", leader: "Giovanni", type: "TIERRA", color: "var(--ground)", poke: "🦏", as: "Rhydon", desc: "Líder de tierra. Débil contra: Agua y Planta (x4). ¡Cuidado con Fisura!" },
-            lavanda: { name: "PUEBLO LAVANDA", leader: "Anaïs", type: "FANTASMA", color: "#705898", poke: "👻", as: "Gastly", desc: "Ciudad tranquila. Aquí se encuentra la Torre Pokémon y el Sr. Fuji." }
+            lavanda: { name: "PUEBLO VILLANUEVA", leader: "Anaïs", type: "HADA", color: "#705898", poke: "🌙", as: "Clefable", desc: "Ciudad tranquila. Te guíaran Lupi,Indi y Simbita a la Torre Pokémon. " }
         };
 
         function updateBattle(key) {
